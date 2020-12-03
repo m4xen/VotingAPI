1e90cea066701c6dd832d2b8182f7298a797f66d
Max Runesson <Max@maxRunesson.se>
2020-12-03 10:25:38 +0100

    api

---
+++ tester.js
@@ -0,0 +1,43 @@
+/*
+	Tester of this Api
+	It will test all the important functions
+	to test if they work properly
+*/
+
+//setting it up
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+var should = require("chai").should()
+
+const baseUrl = "http://127.0.0.1:3000";
+const CheckingData = {
+	"firstname" : "John",
+	"lastname" : "Doe",
+	"id" : 0,
+	"votes": 42,
+	"accomplished" : "Test candidate"
+};
+
+chai.use(chaiHttp);
+// testing starts here
+
+describe("Server", function() {
+	it("running", function(done) {
+		chai.request(baseUrl)
+		.get("/tester")
+		.end(function(err, res) {
+			res.should.have.status(200);
+			done();
+		})
+	});
+});
+describe("Load Candidates", function() {
+	it("Get all Candidates", function(done) {
+		chai.request(baseUrl)
+		.get("/allNobel")
+		.end(function(err, res) {
+			res.should.have.status(200);
+			done();
+		})
+	});
+});
---
+++ voteAPI.js
@@ -0,0 +1,130 @@
+/*
+	Nobel voting API
+	authors: Viktorx64, OscaHar, and M4xen
+*/
+
+console.log("Program starting");
+const express = require('express');
+const router = express()
+const bodyParser = require('body-parser');
+var fs = require('fs');
+
+const dataPathVotes = "./data/votes.json";
+router.get('/tester', function(req, res, next) {
+	res.send("It Works");
+	next();
+});
+
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	next();
+});
+
+//get All Votes
+router.get('/vote', function(req,res, next) {
+	fs.readFile(dataPathVotes, (err, data) => {
+		if (err) {
+			throw err;
+		}
+		//var nobleList = JSON.parse(data);
+		res.send(JSON.parse(data));
+		next();
+
+
+	});
+});
+
+//get poll winner
+router.get('/vote', function(req,res, next) {
+	fs.readFile(dataPathVotes, (err, data) => {
+		if (err) {
+			throw err;
+		}
+		var nobleList = JSON.parse(data);
+		/*res.send(JSON.parse(data));
+		next();*/
+
+		var date = new Date();
+  
+		const lastMonthTooVote = 11;
+		const lastDayTooVote = 10;
+		const lastHourTooVote = 10;
+		const lastMinTooVote = 10;
+
+		if(date.getMonth() + 1 >= lastMonthTooVote && date.getDate() >= lastDayTooVote && date.getHours() >= lastHourTooVote && date.getMinutes() > lastMinTooVote) {
+			console.log("Voting time is over; winner is:");
+			const sort_by = (field, reverse, primer) => {
+
+				const key = primer ?
+				  function(x) {
+					return primer(x[field])
+				  } :
+				  function(x) {
+					return x[field]
+				  };
+			  
+				reverse = !reverse ? 1 : -1;
+			  
+				return function(a, b) {
+				  return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+				}
+			  }
+			  nobleList.sort(sort_by('votes', true, parseInt));
+			  console.log(nobleList[0].name + " " + nobleList[0].votes);
+			  nobleList.sort(sort_by('id', false, parseInt));
+		}
+	});
+});
+
+//save Votes
+router.get('/vote/:id', function(req,res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var nobleList;
+	var id = parseInt(req.params.id);
+	fs.readFile(dataPathVotes, (err, data) => {
+		if (err) {
+			throw err;
+		}
+			
+		var date = new Date();
+  
+		const lastMonthTooVote = 11;
+		const lastDayTooVote = 10;
+		const lastHourTooVote = 10;
+		const lastMinTooVote = 10;
+
+		if(date.getMonth() + 1 >= lastMonthTooVote && date.getDate() >= lastDayTooVote && date.getHours() >= lastHourTooVote && date.getMinutes() > lastMinTooVote) {
+			console.log("Voteing time have expired");
+		}else{
+			canVote();
+		}
+
+		function canVote(){
+			nobleList = JSON.parse(data);
+			nobleList[id].votes++;
+			
+			fs.writeFile(dataPathVotes, JSON.stringify(nobleList), (err) => {
+				if (err) {
+					throw err;
+				}
+				console.log("somebody voted!");
+			});
+			
+			res.send(nobleList);
+			next();
+		}
+	});
+});
+
+/*
+	may remove this when using it as an api
+*/ 
+router.listen(process.env.PORT || 3000, function(){
+	console.log("Server Listening on port 3000");
+});
+//export it
+module.exports = router;
